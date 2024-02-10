4ed9f08b0aa4e4b58599017afeb0fa08e18b65a8
Komal-Prajapat <komalprajapat267@gmail.com>
2024-02-10 18:01:09 +0530

    create router

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BlogPage = () => {
+  return (
+    <div>
+      <h1>Blog Page</h1>
+      <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati optio non accusantium quis maiores eos? Eveniet unde minus obcaecati repellat odit! Hic, dolorum?</h1>
+    </div>
+  )
+}
+
+export default BlogPage
---
+++ src/app/login/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Login PAge
+    </div>
+  )
+}
+
+export default page
